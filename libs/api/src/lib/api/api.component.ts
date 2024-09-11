dee46a6f828b995d7fdf0bdb80cd5d4c3f9c41f6
Vinay Vivekananda <10471989+vinayvivekananda@users.noreply.github.com>
2024-09-12 01:18:33 +0530

    add @bilva/api library

---
+++ libs/api/src/lib/api/api.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-api',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './api.component.html',
+  styleUrl: './api.component.css',
+})
+export class ApiComponent {}
