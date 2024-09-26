6adbb11026e890921a19c41b308d433004bc3a9d
Vinay Vivekananda <10471989+vinayvivekananda@users.noreply.github.com>
2024-09-26 14:36:14 +0530

    remove database related apis

--- apps/server/src/routes/api/index.ts
+++ apps/server/src/routes/api/index.ts
@@ -2,19 +2,19 @@ import express from 'express';
 
 import { authN, adminAuthZ } from '../../handlers/auth';
 
-import { router as account } from './account';
-import { router as session } from './session';
-import { router as swagger } from './swagger';
-import { router as property } from './property';
+// import { router as account } from './account';
+// import { router as session } from './session';
+// import { router as swagger } from './swagger';
+// import { router as property } from './property';
 import { router as callBack } from './request-call-back';
 
 export const router = express.Router();
 
 // Public API
-router.use('/docs', swagger);
-router.use('/account', account);
-router.use('/session', session);
-router.use('/property', property);
+// router.use('/docs', swagger);
+// router.use('/account', account);
+// router.use('/session', session);
+// router.use('/property', property);
 router.use('/request-call-back', callBack);
 
 router.use('/', authN);
