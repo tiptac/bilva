7f090e7de245b83b214136384155efc67479de2b
Vinay Vivekananda <10471989+vinayvivekananda@users.noreply.github.com>
2024-10-27 01:34:20 +0530

    fill bilva slide

--- apps/client/src/app/bilva/bilva-slide/bilva-slide.component.ts
+++ apps/client/src/app/bilva/bilva-slide/bilva-slide.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonText, IonButton } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'bilva-bilva-slide',
   standalone: true,
-  imports: [CommonModule],
+  imports: [IonButton, IonText, CommonModule],
   templateUrl: './bilva-slide.component.html',
   styleUrl: './bilva-slide.component.scss',
 })
