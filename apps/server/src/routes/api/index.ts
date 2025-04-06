dcdca095422e1bcd2786395a3acc34009c0ad6c8
Vinay Vivekananda <10471989+vinayvivekananda@users.noreply.github.com>
2025-04-06 13:45:20 +0530

    Fix assets path

--- apps/server/src/routes/api/index.ts
+++ apps/server/src/routes/api/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import * as path from 'path';
+import serveIndex from 'serve-index';
 
-import { authN, adminAuthZ } from '../../handlers/auth';
+// import { authN, adminAuthZ } from '../../handlers/auth';
 
 // import { router as account } from './account';
 // import { router as session } from './session';
@@ -10,6 +12,13 @@ import { router as callBack } from './request-call-back';
 
 export const router = express.Router();
 
+const assetsPath = path.join(__dirname, '..', '..', 'assets');
+// router.get('/assets', (req, res) => {
+//   res.json({ dirName: __dirname });
+// });
+router.use('/assets', serveIndex(assetsPath));
+router.use('/assets', express.static(assetsPath));
+
 // Public API
 // router.use('/docs', swagger);
 // router.use('/account', account);
@@ -17,8 +26,8 @@ export const router = express.Router();
 // router.use('/property', property);
 router.use('/request-call-back', callBack);
 
-router.use('/', authN);
+// router.use('/', authN);
 // AuthN API
 
-router.use('/', adminAuthZ);
+// router.use('/', adminAuthZ);
 // Admin AuthZ API
