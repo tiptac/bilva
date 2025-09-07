e9a1861df5fcfeb59ce79bc2d92ce65c0a24ecd2
Vinay Vivekananda <10471989+vinayvivekananda@users.noreply.github.com>
2025-09-07 20:07:24 +0530

    move details to .ts

--- apps/client/src/app/landing/bilva-landing/business-vertical/realty-solution/own-it/own-it.component.ts
+++ apps/client/src/app/landing/bilva-landing/business-vertical/realty-solution/own-it/own-it.component.ts
@@ -18,6 +18,43 @@ export class OwnItComponent implements AfterViewInit {
 
   appRoutes = appRoutes;
 
+  solutions = [
+    {
+      imgSrc: 'assets/images/BS.WEBP',
+      title: 'Buying and Selling',
+      content:
+        'Making buying and selling effortless, transparent, and worry-free.',
+      charges: ['1% from Buyer / Seller / Both'],
+    },
+    {
+      imgSrc: 'assets/images/HS.WEBP',
+      title: 'Home Construction',
+      content:
+        'Creating timeless spaces with elegance, lasting quality, and fair pricing.',
+      charges: ['Turnkey - Rs1950/SqFt', 'Hybrid - Rs350/SqFt'],
+    },
+    {
+      imgSrc: 'assets/images/HI.webp',
+      title: 'Home Interiors',
+      content:
+        'Smart yet stylish spaces designed to enrich your everyday life.',
+      charges: ['Tailored, once we know your vibe!'],
+    },
+    {
+      imgSrc: 'assets/images/AM.webp',
+      title: 'Asset Management',
+      content:
+        'Effortless property care that safeguards value and amplifies returns.',
+      charges: ['10% of monthly rental / Pay-Per-Use'],
+    },
+    {
+      imgSrc: 'assets/images/RS.WEBP',
+      title: 'Relocation Support',
+      content: 'Simplifying your shift with expert care every step of the way.',
+      charges: ['Price tailored to your move'],
+    },
+  ];
+
   ngAfterViewInit(): void {
     setTimeout(() => {
       const testimonialsTxt = document.getElementById('testimonialsTxt');
